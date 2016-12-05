a0c2d39c5af240e77cb3d3092eac45b15dc06d0f
tckz916 <Souya0916>
2016-12-06 04:09:40 +0900

    [UPDATE] make.bat

--- docs/make.bat
+++ docs/make.bat
@@ -127,9 +127,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\JapanMinecraftPvPDocumentation.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\BindunityNetworkDocumentation.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\JapanMinecraftPvPDocumentation.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\BindunityNetworkDocumentation.ghc
 	goto end
 )
 
